936e072f28ec359a09343b7f3b651ca19c6d2cdc
Bobby Sixkiller <jansoldat68@gmail.com>
2020-01-28 14:37:57 +0100

    Edited responsivness of the main page, added menu

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 import "./main.scss";
+import { log } from "util";
 
-console.log('Hello Costa Paradios')
+/* Navbar */
+
+const showcase = document.querySelector('.showcase');
+
+const navbar = {
+    btn: document.querySelector('.navbar__icon'),
+    content: document.querySelector('.navbar'),
+    toggleState: function() {
+        this.btn.classList.toggle('active');
+        this.content.classList.toggle('active');
+    }
+}
+
+navbar.btn.addEventListener('click', () => {
+    navbar.toggleState();
+    showcase.classList.toggle('active');
+
+});
