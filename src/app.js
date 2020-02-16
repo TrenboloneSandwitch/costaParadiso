35bd95d61f742fd48d9d50dd47de5c578201dca2
Kwai <kwai@Kwai-MBP.mshome.net>
2020-02-16 21:48:04 +0100

    Reworked animation, version 1.0

--- src/app.js
+++ src/app.js
@@ -7,11 +7,17 @@ import {
     ScrollMaster
 } from "./js/scrollMaster.js";
 
-import { Gallery } from "./js/gallery";
-import { Lazy } from "./js/lazyLoader";
+import {
+    Gallery
+} from "./js/gallery";
+import {
+    Lazy
+} from "./js/lazyLoader";
 
 
-
+import {
+    gsap
+} from "gsap";
 
 import '../assets/img/sprite.svg';
 
@@ -20,6 +26,11 @@ import '../assets/img/sprite.svg';
 const showcase = document.querySelector('.showcase');
 
 const form = document.querySelector(".form");
+
+const bImg = document.querySelectorAll('.beaches__img')[2];
+const bIco = document.querySelector('.beaches__icon');
+
+
 
 const ul = document.querySelector('.at__list');
 const row2 = document.querySelector('.attributes__row--2');
@@ -31,37 +42,43 @@ navlinks.forEach(nl => {
     nl.addEventListener('click', () => {
         navbar.toggleState();
         showcase.classList.toggle('active');
+        tl.reversed() ? tl.play() : tl.reverse();
     });
 });
 
 window.addEventListener('resize', () => {
     placeApartmentList();
     resizeQuoteBoxes();
+
+    bIco.style.height = bImg.offsetHeight + 'px';
+
 });
 
-const previewImgs = [], src = [];
+const previewImgs = [],
+    src = [];
 const classes = {
     'vertical': [1, 6],
-    'horizontal': [9],
-    'big': [2]
+    'horizontal': [9, 22, 30],
+    'big': [2, 15]
 };
 
 window.addEventListener('load', () => {
     placeApartmentList();
     resizeQuoteBoxes();
+    // resize icon in nearby section
+    bIco.style.height = bImg.offsetHeight + 'px';
+    bIco.style.width = bImg.offsetWidth + 'px';
 
-    
-    for (let index = 0; index < 10; index++) {
-        previewImgs.push(`./assets/img/byt1.jpg`);
-        src.push(`assets/img/byt1.jpg`);
-        
+    for (let index = 0; index < 31; index++) {
+        previewImgs.push(`./assets/img/gallery/${index}.jpg`);
+        src.push(`assets/img/gallery/${index}.jpg`);
     }
 
-	new Gallery('.gallery-container', previewImgs, src, classes);
-
-	new Lazy();
-
+    new Gallery('.gallery-container', previewImgs, src, classes);
+    new Lazy();
 });
+
+
 
 window.addEventListener('scroll', (e) => {
     const target = document.querySelector('.showcase__content');
@@ -70,9 +87,9 @@ window.addEventListener('scroll', (e) => {
         const windowHeight = window.innerHeight;
         const targetHeight = target.offsetHeight;
 
-		if (windowHeight / 3 - targetHeight > window.pageYOffset) {
-	        requestAnimationFrame(an);
-		}
+        if (windowHeight / 3 - targetHeight > window.pageYOffset) {
+            requestAnimationFrame(an);
+        }
     }
 
     function an() {
@@ -84,7 +101,7 @@ window.addEventListener('scroll', (e) => {
 
 document.addEventListener('DOMContentLoaded', () => {
     new formValidation(form);
-    const smoothScroll = new ScrollMaster(1000, '.navbar__link');
+    const smoothScroll = new ScrollMaster(1000, '.smoothScroll');
     smoothScroll.init();
 })
 
@@ -124,9 +141,10 @@ function placeApartmentList() {
         row2.appendChild(ul);
     }
 
-    const cnt =  document.querySelector('.showcase__content');
-    if( w <= 568 && cnt.style.transform !== `translate3d(-50%, -50%, 0px)`) {cnt.style.transform = `translate3d(-50%, -50%, 0px)`;}
-    else if(w > 568 ){
+    const cnt = document.querySelector('.showcase__content');
+    if (w <= 568 && cnt.style.transform !== `translate3d(-50%, -50%, 0px)`) {
+        cnt.style.transform = `translate3d(-50%, -50%, 0px)`;
+    } else if (w > 568) {
         cnt.style.transform = `translate3d(0, 0, 0px)`;
     }
 }
@@ -140,8 +158,35 @@ const navbar = {
     }
 }
 
+
+const tl = gsap.timeline();
+tl.to('.navbar', {
+        height: '100%',
+        width: '300px',
+        duration: 0.6,
+        borderRadius: "0 0 0 10em"
+    })
+    .to(".showcase__content", {
+        opacity: 0,
+        y: 120,
+        duration: 1
+    })
+    .to('.navbar__link', {
+        display: 'block',
+        duration: 0.1
+    }, "-=1")
+    .to('.navbar__item', {
+        opacity: 1,
+        duration: 0.2,
+        stagger: 0.2
+    }, "-=0.6")
+
+    .reverse();
+
 navbar.btn.addEventListener('click', () => {
     navbar.toggleState();
-    showcase.classList.toggle('active');
 
+
+    tl.reversed() ? tl.play() : tl.reverse();
+    showcase.classList.toggle('active');
 });
