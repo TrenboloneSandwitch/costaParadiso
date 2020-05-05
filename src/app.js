cc00cf0510b3d089aa1838ab7657fbc37fafaf20
Bobby Sixkiller <soldier068@email.cz>
2020-05-05 14:33:13 +0200

    Refactor, added it language,minor css changes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 import "./main.scss";
-
 import { formValidation } from "./js/formValidation.js";
 import { ScrollMaster } from "./js/scrollMaster.js";
-
 import { Gallery } from "./js/gallery";
 import { Lazy } from "./js/lazyLoader";
 import { Language } from "./js/language";
-
 import { gsap } from "gsap";
-
 import getJson from "./js/httpHandler";
-
 import "../assets/img/sprite.svg";
 
-/* Navbar */
 
+// Vars Initializations
+const previewImgs = [], src = [];
+const classes = { vertical: [1, 6], horizontal: [9, 20], big: [2, 15] };
+const tl = gsap.timeline();
+// UI vars
 const showcase = document.querySelector(".showcase");
+const showcase__content = document.querySelector(".showcase__content");
 const form = document.querySelector(".form");
 const bImg = document.querySelectorAll(".beaches__img")[2];
 const bIco = document.querySelector(".beaches__icon");
@@ -23,48 +23,103 @@ const ul = document.querySelector(".at__list");
 const row2 = document.querySelector(".attributes__row--2");
 const row3 = document.querySelector(".attributes__row--3");
 const navlinks = document.querySelectorAll(".navbar__link");
-
-navlinks.forEach(nl => {
-  nl.addEventListener("click", () => {
-    navbar.toggleState();
-    showcase.classList.toggle("active");
-    tl.reversed() ? tl.play() : tl.reverse();
-  });
-});
-
-window.addEventListener("resize", () => {
-  placeApartmentList();
-  resizeQuoteBoxes();
-
-  bIco.style.height = bImg.offsetHeight + "px";
-});
-
-const previewImgs = [],
-  src = [];
-const classes = {
-  vertical: [1, 6],
-  horizontal: [9, 20],
-  big: [2, 15]
+const navbar = {
+  btn: document.querySelector(".navbar__icon"),
+  content: document.querySelector(".navbar"),
+  toggleState: function() {
+    this.btn.classList.toggle("active");
+    this.content.classList.toggle("active");
+  }
 };
 
-window.addEventListener("load", () => {
-  placeApartmentList();
-  resizeQuoteBoxes();
-  // resize icon in nearby section
-  bIco.style.height = bImg.offsetHeight + "px";
-  bIco.style.width = bImg.offsetWidth + "px";
+// Functions
+function resizeQuoteBoxes() {
+  const quotes = document.querySelectorAll(".quote");
+  quotes.forEach(q => {
+    const text = q.querySelector(".quote__text");
+    const box = q.querySelector(".quote__box");
 
-  for (let index = 1; index < 22; index++) {
-    previewImgs.push(`./assets/img/gallery/${index}.jpg`);
-    src.push(`assets/img/gallery/${index}.jpg`);
+    box.style.height = text.offsetHeight + "px";
+    box.style.width = text.offsetHeight + "px";
+  });
+}
+
+function resizeShowcaseContent(width) {
+        if (width < 568 && showcase__content.style.transform !== `translate3d(-50%, -50%, 0px)`) {
+            showcase__content.style.transform = `translate3d(-50%, -50%, 0px)`;
+        }
+        else if (width >= 568) {
+            showcase__content.style.transform = `translate3d(0, 0, 0px)`;
+        }
+}
+
+function placeApartmentList(width) {
+  if (width >= 1024 && row3.childElementCount === 0) {
+    ul.remove();
+    row3.innerHTML = `
+        <div class="attributes__element at__list">
+            <ul class="attributes__list">
+                <li data-lang="26">kuchyňská linka je vybavena</li>
+				<li data-lang="27">koupelna bez pračky</li>
+            </ul>
+        </div>
+        <div class="attributes__element at__list">
+            <ul class="attributes__list">
+                <li data-lang="28">4 lůžka + 1 přistýlka </li>
+				<li data-lang="29">přístup není bezbariérový</li>
+            </ul>
+        </div>
+        `;
+  }
+    
+  if (width < 1024 && row3.childElementCount !== 0) {
+    row3.innerHTML = "";
+    row2.appendChild(ul);
   }
 
-  new Gallery(".gallery-container", previewImgs, src, classes);
-  new Lazy();
+//   if (!localStorage.getItem("lang")) return;
+//   const lang = localStorage.getItem("lang");
+//   const filePath = `../../assets/data/translation-${lang}.json`;
+//   getJson(filePath).then(data => {
+//     document.querySelectorAll("[data-lang]").forEach(singleEl => {
+//       const key = singleEl.getAttribute("data-lang");
+//       singleEl.innerText = data[key];
+//     });
+//   });
+}
 
-  tl.to(".navbar", {
+function checkLanguage(params) {
+    if (!localStorage.getItem("lang")) return;
+    const lang = localStorage.getItem("lang");
+    const filePath = `../../assets/data/translation-${lang}.json`;
+    setLanguage(filePath);
+    
+}
+
+function setLanguage(filePath) {
+    getJson(filePath).then(data => {
+        document.querySelectorAll("[data-lang]").forEach(singleEl => {
+        const key = singleEl.getAttribute("data-lang");
+        singleEl.innerText = data[key];
+        });
+    });
+}
+
+function resizedBasedTasks() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    placeApartmentList(width);
+    resizeShowcaseContent(width);
+    resizeQuoteBoxes();
+    checkLanguage();
+
+    // resize icon in nearby section
+    bIco.style.height = bImg.offsetHeight + "px";
+    bIco.style.width = bImg.offsetWidth + "px";
+}
+function timelineInit() {
+    tl.to(".navbar", {
     height: "100%",
-    width: "300px",
+    width: '350px',
     duration: 0.6,
     borderRadius: "0 0 0 10em"
   })
@@ -91,103 +146,66 @@ window.addEventListener("load", () => {
     )
 
     .reverse();
-});
+}
 
-window.addEventListener("scroll", e => {
-  const target = document.querySelector(".showcase__content");
+function navbarToggle() {
+    navbar.toggleState();
+    showcase.classList.toggle("active");
+    tl.reversed() ? tl.play() : tl.reverse();
+}
+
+// Event Listeners
+    // Resize
+window.addEventListener("resize", () => {
+    resizedBasedTasks();
+});
+// Scroll   
+window.addEventListener("scroll", () => {
   if (window.innerWidth >= 568) {
     const windowHeight = window.innerHeight;
-    const targetHeight = target.offsetHeight;
+    const targetHeight = showcase__content.offsetHeight;
 
     if (windowHeight / 3 - targetHeight > window.pageYOffset) {
-      requestAnimationFrame(an);
+      requestAnimationFrame(animate);
     }
   }
 
-  function an() {
-    let scrolled = window.pageYOffset;
-    const rate = scrolled * 2.3;
-    target.style.transform = `translate3d(0px, ${rate}px, 0px)`;
-  }
+    function animate() {
+        let scrolled = window.pageYOffset;
+        const rate = scrolled * 2.3;
+        showcase__content.style.transform = `translate3d(0px, ${rate}px, 0px)`;
+    }
 });
 
+// DOM Loaded
 document.addEventListener("DOMContentLoaded", () => {
   new formValidation(form);
   const smoothScroll = new ScrollMaster(1000, ".smoothScroll");
   smoothScroll.init();
-  new Language(".flag-icon", "A ");
+  new Language(".flag-icon");
 });
 
-function resizeQuoteBoxes() {
-  const quotes = document.querySelectorAll(".quote");
-  quotes.forEach(q => {
-    const text = q.querySelector(".quote__text");
-    const box = q.querySelector(".quote__box");
+// Window Load
+window.addEventListener("load", () => {
+    resizedBasedTasks();
+    timelineInit();
+    // Gallery images init
+    for (let index = 1; index < 22; index++) {
+        previewImgs.push(`./assets/img/gallery/${index}.jpg`);
+        src.push(`assets/img/gallery/${index}.jpg`);
+    }
+    new Gallery(".gallery-container", previewImgs, src, classes);
+    new Lazy();
+});
 
-    box.style.height = text.offsetHeight + "px";
-    box.style.width = text.offsetHeight + "px";
-  });
-}
-
-function placeApartmentList() {
-  const w =
-    window.innerWidth ||
-    document.documentElement.clientWidth ||
-    document.body.clientWidth;
-  if (w >= 1024 && row3.childElementCount === 0) {
-    ul.remove();
-    row3.innerHTML = `
-        <div class="attributes__element at__list">
-        <ul class="attributes__list">
-        <li data-lang="26">kuchyňská linka je vybavena</li>
-				<li data-lang="27">koupelna bez pračky</li>
-        </ul>
-        </div>
-        <div class="attributes__element at__list">
-        <ul class="attributes__list">
-        <li data-lang="28">4 lůžka + 1 přistýlka </li>
-				<li data-lang="29">přístup není bezbariérový</li>
-        </ul>
-        </div>
-        `;
-  }
-  if (w < 1024 && row3.childElementCount !== 0) {
-    row3.innerHTML = "";
-    row2.appendChild(ul);
-  }
-
-  const cnt = document.querySelector(".showcase__content");
-  if (w < 568 && cnt.style.transform !== `translate3d(-50%, -50%, 0px)`) {
-    cnt.style.transform = `translate3d(-50%, -50%, 0px)`;
-  } else if (w >= 568) {
-    cnt.style.transform = `translate3d(0, 0, 0px)`;
-  }
-
-  if (!localStorage.getItem("lang")) return;
-  const lang = localStorage.getItem("lang");
-  const filePath = `../../assets/data/translation-${lang}.json`;
-  getJson(filePath).then(data => {
-    document.querySelectorAll("[data-lang]").forEach(singleEl => {
-      const key = singleEl.getAttribute("data-lang");
-      singleEl.innerText = data[key];
-    });
-  });
-}
-
-const navbar = {
-  btn: document.querySelector(".navbar__icon"),
-  content: document.querySelector(".navbar"),
-  toggleState: function() {
-    this.btn.classList.toggle("active");
-    this.content.classList.toggle("active");
-  }
-};
-
-const tl = gsap.timeline();
-
+// Navbar button click
 navbar.btn.addEventListener("click", () => {
-  navbar.toggleState();
+    navbarToggle();
+});
 
-  tl.reversed() ? tl.play() : tl.reverse();
-  showcase.classList.toggle("active");
+// Navlinks click
+navlinks.forEach(nl => {
+  nl.addEventListener("click", () => {
+      navbarToggle();
+  });
 });
--- src/js/language.js
+++ src/js/language.js
@@ -15,7 +15,7 @@ export class Language {
   }
 
   init() {
-    this.placeFlag();
+    this.placeFlags();
     this.translate(this.lang);
   }
 
@@ -23,31 +23,72 @@ export class Language {
     return localStorage.getItem("lang") ? localStorage.getItem("lang") : "cz";
   }
 
-  getFlag() {
+  getFlags() {
     this.lang = this.getLanguage();
-    const lang = this.lang === "gb" ? "cz" : "gb";
-    return `
-        <a href='#' class="flag-icon flag-icon-${lang}"></a>
-        `;
+    
+    let lang = [];
+    if (this.lang === "gb") lang = ['cz', 'it'];
+    if (this.lang === "cz") lang = ['gb', 'it'];
+    if (this.lang === "it") lang = ['cz', 'gb'];
+    const flags = lang.map(l => (`<a href='#' class="flag-icon flag-icon-${l}"></a>`));
+    return flags;
   }
 
   changeFlag() {
-    const flagContainer = document.querySelector(this.flagContainer);
-    flagContainer.addEventListener("click", e => {
-      e.preventDefault();
-      const classes = e.target.classList;
-      const lang = classes.contains("flag-icon-gb") ? "gb" : "cz";
+    const flagContainers = document.querySelectorAll('.flag-icon');
 
-      localStorage.setItem("lang", lang);
-      this.placeFlag();
-      this.translate(lang);
-    });
+    flagContainers.forEach(f => {
+      f.addEventListener("click", e => {
+        e.preventDefault();
+        const classes = e.target.classList;
+        let lang;
+        if (classes.contains("flag-icon-gb")) lang = 'gb';
+        if (classes.contains("flag-icon-cz")) lang = 'cz';
+        if (classes.contains("flag-icon-it")) lang = 'it';
+        
+        localStorage.setItem("lang", lang);
+        this.placeFlags();
+        this.translate(lang);
+      });
+      
+    })
   }
 
-  placeFlag() {
+
+  placeFlags() {
     const flagParent = document.querySelector(".lang-flag");
-    flagParent.innerHTML = this.getFlag();
+    flagParent.innerHTML = this.getFlags();
     this.changeFlag();
+  }
+
+  getFormTranslation(lang) {
+    let form = {};
+    if (lang === 'cz') form = {
+            name: "Jméno a Příjmení",
+            subject: "Předmět",
+            email: "vas@e-mail.cz",
+            year: "Aktuální Rok",
+            msg: "Text Vaší zprávy...",
+            submit: "Odeslat"
+    }
+    if (lang === 'gb') form = {
+            name: "First name & Surname",
+            subject: "Subject",
+            email: "your@email-address.com",
+            year: "Current Year",
+            msg: "Your message...",
+            submit: "Send"
+    }
+     if (lang === 'it') form = {
+            name: "Nome e Cognome",
+            subject: "Oggetto",
+            email: "vostro@mail.eu",
+            year: "Anno attuale",
+            msg: "Testo di messaggio...",
+            submit: "Inviare"
+     }
+    
+    return form;
   }
 
   async translate(lang) {
@@ -60,27 +101,17 @@ export class Language {
       singleEl.innerText = translatedData[key];
     });
 
-    const form =
-      lang === "cz"
-        ? {
-            name: "Jméno a Příjmení",
-            subject: "Předmět",
-            email: "vas@e-mail.cz",
-            year: "Aktuální Rok",
-            msg: "Text Vaší zprávy...",
-            submit: "Odeslat"
-          }
-        : {
-            name: "Name & Surname",
-            subject: "Subject",
-            email: "your@email.com",
-            year: "Current Year",
-            msg: "Text of your message...",
-            submit: "Send"
-          };
+    const form = this.getFormTranslation(lang);
 
+  
     Object.keys(this.form).forEach(key => {
+      
+      if (this.form[key].type === 'submit') {
+        this.form[key].value = form[key];
+        return;
+      }
       this.form[key].placeholder = form[key];
+      
     });
   }
 }
